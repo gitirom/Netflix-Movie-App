e162eb040c1f808cd945e228cf7594552b25df1c
gitirom <wadiaromdane10@gmail.com>
2023-04-21 01:35:08 +0200

    i create the featured part

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,10 +18,10 @@ const Navbar = () => {
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
                     alt="" />
                     <span>Homepage</span>
-                    <span>Homepage</span>
-                    <span>Homepage</span>
-                    <span>Homepage</span>
-                    <span>Homepage</span>
+                    <span>Series</span>
+                    <span>Movies</span>
+                    <span>New and Popular</span>
+                    <span>My List</span>
                 </div>
                 <div className="right">
                     <Search className="icon" />
