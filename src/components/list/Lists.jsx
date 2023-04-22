85f48dc1f053f9ee19e72e76b722e60249a8ece0
gitirom <wadiaromdane10@gmail.com>
2023-04-22 14:54:37 +0200

    I add the slideNumber function , transition for the list when moved and appear and disappear function for the icon

--- src/components/list/Lists.jsx
+++ src/components/list/Lists.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import ListItem from '../listItem/ListItem';
 import './list.scss';
 import {
@@ -7,19 +8,35 @@ import {
 
 
 const Lists = () => {
+    const [isMoved, setisMoved] = useState(false);                 //these state for appear and disappear the left icon 
+    
+    const [slideNumber, setSlideNumber] = useState(0);
+
+    const ListRef = useRef();                         //Like getElementById but with jsx
+    
+    const handleClick = (direction) => {
+        setisMoved(true);
+        let distance = ListRef.current.getBoundingClientRect().x - 50;       //these function gives you the nb of px equal to distance      //these method to now how many px 
+        
+    if (direction === "left" && slideNumber > 0) {
+        setSlideNumber(slideNumber - 1);
+        ListRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 5) {
+        setSlideNumber(slideNumber + 1);
+        ListRef.current.style.transform = `translateX(${-230 + distance}px)`;
+        }
+        
+    }
+
     return (
         <div className='list' >
             <span className="listTitle">Continue to watch </span>
             <div className="wrapper">
-                <ArrowBackIosOutlined className='sliderArrow left' />
-                <div className="container">
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
-                    <ListItem />
+                <ArrowBackIosOutlined className='sliderArrow left' onClick={() => handleClick("left")} 
+                style={{display: !isMoved && "none"}} 
+                />
+                <div className="container" ref={ListRef}>
                     <ListItem />
                     <ListItem />
                     <ListItem />
@@ -31,7 +48,7 @@ const Lists = () => {
                     <ListItem />
                     <ListItem />
                 </div>
-                <ArrowForwardIosOutlined className='sliderArrow right' />
+                <ArrowForwardIosOutlined className='sliderArrow right' onClick={() => handleClick("right")} />
             </div>
         </div>
     );
