e5f16bb027bc37db06ccd99a874a5a82f2f5689c
gitirom <wadiaromdane10@gmail.com>
2023-06-26 15:08:24 +0200

    I start the Backend Creating the models

---
+++ api/models/Lists.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+
+const ListSchema = new mongoose.Schema(
+    {
+        title:{type: String, required: true, unique: true},
+        genre:{type: String },
+        type:{type: String },
+        content:{type: Array },
+        
+
+    }, 
+    { timestamps :true }
+);
+
+module.export = mongoose.model("List", MovieSchema);
